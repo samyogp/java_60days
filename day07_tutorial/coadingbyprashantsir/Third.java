c723131a72cd053cc05d5422caaba60f9b88d454
samyogp <samyogphuyal036@gmail.com>
2026-02-17 21:43:12 +0545

    second project to print subscribe in terminal

---
+++ day07_tutorial/coadingbyprashantsir/Third.java
@@ -0,0 +1,11 @@
+package day07_tutorial.coadingbyprashantsir;
+
+// create a new project in inteijij idea and output "subscribe" on the Console 
+
+public class Third {
+    public static void main(String[] args) {
+        System.out.println("Subscribe to Phuyal creates");
+
+    }
+    
+}
