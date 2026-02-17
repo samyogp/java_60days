c82017c7fa581229a921ec4c7dbc0317faa7deac
samyogp <samyogphuyal036@gmail.com>
2026-02-17 21:46:40 +0545

    first pattern created using simple java program

---
+++ day07_tutorial/coadingbyprashantsir/FirstPattern.java
@@ -0,0 +1,14 @@
+package day07_tutorial.coadingbyprashantsir;
+
+public class FirstPattern {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * *");
+        System.out.println("* * * *");
+      System.out.println("* * * * *");
+    
+    
+    }
+    
+}
---
+++ day07_tutorial/coadingbyprashantsir/SecondPattern.java
@@ -0,0 +1,5 @@
+package day07_tutorial.coadingbyprashantsir;
+
+public class SecondPattern {
+    
+}
