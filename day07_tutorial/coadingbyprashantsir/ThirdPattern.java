6fbb4c62c9b70cc0a7dcbdd5aad4f0090e61aafa
samyogp <samyogphuyal036@gmail.com>
2026-02-17 21:54:48 +0545

    add third pattern java using println statement

---
+++ day07_tutorial/coadingbyprashantsir/ThirdPattern.java
@@ -0,0 +1,13 @@
+package day07_tutorial.coadingbyprashantsir;
+// Right half piramid pattern using println satement 
+public class ThirdPattern {
+    public static void main(String[] args) {
+        System.out.println("       *");
+        System.out.println("     * *");
+        System.out.println("   * * *");
+        System.out.println(" * * * *");
+        System.out.println("* * * * *");
+
+    }
+    
+}
