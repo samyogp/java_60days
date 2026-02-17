11115401a91e143fab68420045c8be0849021ff0
samyogp <samyogphuyal036@gmail.com>
2026-02-17 21:50:03 +0545

    right half piramid using simple code and also using the work of println

--- day07_tutorial/coadingbyprashantsir/SecondPattern.java
+++ day07_tutorial/coadingbyprashantsir/SecondPattern.java
@@ -1,5 +1,12 @@
 package day07_tutorial.coadingbyprashantsir;
-
+// right half piramid pattern 
 public class SecondPattern {
+    public static void main(String[] args) {
+        System.out.println("     *");
+        System.out.println("    * *");
+        System.out.println("   * * *");
+        System.out.println("  * * * *");
+        System.out.println(" * * * * *");
+    }
     
 }
