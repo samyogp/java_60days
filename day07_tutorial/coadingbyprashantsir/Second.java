dec7ae24715bc8befbd3eee24685e4626f3dbd9f
samyogp <samyogphuyal036@gmail.com>
2026-02-17 20:27:40 +0545

    first coading of the tutorial good morning

---
+++ day07_tutorial/coadingbyprashantsir/Second.java
@@ -0,0 +1,9 @@
+package day07_tutorial.coadingbyprashantsir;
+ // create a class to output "good morning" using a text editor and check output
+
+public class Second {
+    public static void main(String[] args) {
+        System.out.println("Good Morning");
+    }
+    
+}
