0d5358a3bc45d5b080f5184068729597aa123008
samyogp <samyogphuyal036@gmail.com>
2026-02-17 17:40:27 +0545

    basic examples via tutorials

--- day07_tutorial/coadingbyprashantsir/firstprogram.java
+++ day07_tutorial/coadingbyprashantsir/firstprogram.java
@@ -4,6 +4,18 @@ package day07_tutorial.coadingbyprashantsir;
 public class firstprogram {
     public static void main(String[] args) {
         System.out.println("Welcome to phuyal creates java tutorials!");
+     // example of print vs println
+     System.out.println("one");
+     System.out.println("two");
+     System.out.println("three");
+     System.out.print("one");
+      System.out.print("Two");
+    System.out.print("Three");
+
+   
+   
+   
+   
     }
     
 }
