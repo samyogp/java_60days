bbad2e0d046328bff9a814d1659c1382b3344e22
samyogp <samyogphuyal036@gmail.com>
2026-02-18 18:19:51 +0545

    adding psvm in code

--- day12_polymorphism/CompileTimePolymorphism/ConstructorOverloadingDemo.java
+++ day12_polymorphism/CompileTimePolymorphism/ConstructorOverloadingDemo.java
@@ -13,7 +13,10 @@ class Student {
 
 
 public class ConstructorOverloadingDemo {
-    public static void main(String[] args) {
+public static void main(String[] args) {
+    
+ 
+
         Student s1 = new Student();
         Student s2 = new Student("Samyog");
     }
