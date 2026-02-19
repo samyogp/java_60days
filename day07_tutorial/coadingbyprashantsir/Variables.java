7dc2977f51240db1b24e7e171d187d97e331e59d
samyogp <samyogphuyal036@gmail.com>
2026-02-19 19:59:52 +0545

    updated variables code

--- day07_tutorial/coadingbyprashantsir/Variables.java
+++ day07_tutorial/coadingbyprashantsir/Variables.java
@@ -4,12 +4,20 @@ package day07_tutorial.coadingbyprashantsir;
 public class Variables {
     public static void main(String[] args) {
         int myNumber = 865;
-        System.out.println("myNumber is: " + myNumber);
-
-
         int YourNumber;
+
+        System.out.println("YourNumber is: " + myNumber); // this will give an error because we have not initialized the variable yet
+
+
+    
         YourNumber = 500;
         System.out.println("YourNumber is: " + YourNumber);
+
+
+        float myFloat = 3.0f;
+        boolean isVegetarian = true;
+        System.out.println("isVegeterian: " + isVegetarian);
+
 
     }
 
