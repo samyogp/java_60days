7d04c826ee423e51ec9186b1c8917ea2e4236fa0
samyogp <samyogphuyal036@gmail.com>
2026-02-19 19:52:09 +0545

    another examples of variables

---
+++ day07_tutorial/coadingbyprashantsir/Variables.java
@@ -0,0 +1,17 @@
+package day07_tutorial.coadingbyprashantsir;
+
+
+public class Variables {
+    public static void main(String[] args) {
+        int myNumber = 865;
+        System.out.println("myNumber is: " + myNumber);
+
+
+        int YourNumber;
+        YourNumber = 500;
+        System.out.println("YourNumber is: " + YourNumber);
+
+    }
+
+
+}
