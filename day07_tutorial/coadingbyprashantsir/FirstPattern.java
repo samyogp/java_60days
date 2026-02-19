f6109604d27f400f262bbed6d9db9d2f86540c63
samyogp <samyogphuyal036@gmail.com>
2026-02-19 19:19:13 +0545

    updated code

--- day07_tutorial/coadingbyprashantsir/FirstPattern.java
+++ day07_tutorial/coadingbyprashantsir/FirstPattern.java
@@ -2,11 +2,11 @@ package day07_tutorial.coadingbyprashantsir;
 
 public class FirstPattern {
     public static void main(String[] args) {
-        System.out.println("*");
-        System.out.println("* *");
-        System.out.println("* * *");
-        System.out.println("* * * *");
-      System.out.println("* * * * *");
+        System.out.println(" *");
+        System.out.println(" * *");
+        System.out.println(" * * *");
+        System.out.println(" * * * *");
+      System.out.println("   * * * * *");
     
     
     }
