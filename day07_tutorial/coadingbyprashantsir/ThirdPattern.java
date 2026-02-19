149d5c13abceb2c5e6a42952c1213b655d205c14
samyogp <samyogphuyal036@gmail.com>
2026-02-19 21:21:25 +0545

    adding new comment in program

--- day07_tutorial/coadingbyprashantsir/ThirdPattern.java
+++ day07_tutorial/coadingbyprashantsir/ThirdPattern.java
@@ -2,6 +2,8 @@ package day07_tutorial.coadingbyprashantsir;
 // Right half piramid pattern using println satement 
 public class ThirdPattern {
     public static void main(String[] args) {
+
+        // right half piramid pattern using println satement
         System.out.println("       *");
         System.out.println("     * *");
         System.out.println("   * * *");
