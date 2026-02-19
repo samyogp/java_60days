6edac08e8875fefc24c935ab82b39c5ccc4a8662
samyogp <samyogphuyal036@gmail.com>
2026-02-19 19:25:41 +0545

    Adding new program of variables

---
+++ day07_tutorial/coadingbyprashantsir/VariablesDemo.java
@@ -0,0 +1,16 @@
+package day07_tutorial.coadingbyprashantsir;
+
+// its a variable which can store data and we can use it later in our program
+
+public class VariablesDemo {
+    public static void main(String[] args) {
+        int age = 18; // variable declaration and initialization
+        String name = "Samyog"; // variable declaration and initialization
+        double salary = 50000.50; // variable declaration and initialization
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+    }
+    
+}
--- day12_polymorphism/CompileTimePolymorphism/ConstructorOverloadingDemo.java
+++ day12_polymorphism/CompileTimePolymorphism/ConstructorOverloadingDemo.java
@@ -19,6 +19,7 @@ public static void main(String[] args) {
 
         Student s1 = new Student();
         Student s2 = new Student("Samyog");
+        
     }
 
     
