9db73750ca0f697d862e40d170f07d3edd72f795
samyogp <samyogphuyal036@gmail.com>
2026-02-20 18:11:51 +0545

    printing pattern using \n

---
+++ day07_tutorial/coadingbyprashantsir/Challenge1.java
@@ -0,0 +1,11 @@
+package day07_tutorial.coadingbyprashantsir;
+
+public class Challenge1 {
+    // perfect example of escape sequence in java to go next line and print the pattern of stars in the output.
+
+    public static void main(String[] args) {
+        System.out.println("*\n**\n***\n****\n*****");
+
+    }
+    
+}
