8979ced48dac4037f7b70240ec4b3aa5aa52ddfb
samyogp <samyogphuyal036@gmail.com>
2026-02-20 18:19:12 +0545

    left half pyramid example added in the code

--- day07_tutorial/coadingbyprashantsir/Challenge1.java
+++ day07_tutorial/coadingbyprashantsir/Challenge1.java
@@ -4,10 +4,17 @@ public class Challenge1 {
     // perfect example of escape sequence in java to go next line and print the pattern of stars in the output.
 
     public static void main(String[] args) {
+        // Right half Pyramid.
         System.out.println("*\n**\n***\n****\n*****");
 
-        // reverse pattern of stars in the output.
+
+
+        // reverse right half pyramid of stars in the output.
         System.out.println("*****\n****\n***\n**\n*");
+
+        // Left Half Pyramid.
+        System.out.println("  *\n **\n ***\n ****\n *****");
+        
 
 
     }
