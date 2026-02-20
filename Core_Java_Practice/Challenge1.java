d3123921bfee63098a3ce9a2e1233acc14eddf0c
samyogp <samyogphuyal036@gmail.com>
2026-02-20 23:13:50 +0545

    Creating new folder the name is Core_Java_Practice and doing challenge1 that given from youtube tutorial

---
+++ Core_Java_Practice/Challenge1.java
@@ -0,0 +1,25 @@
+package Core_Java_Practice;
+
+public class Challenge1 {
+   
+    public static void main(String[] args) {
+        // Right half Pyramid.
+        System.out.println("*\n**\n***\n****\n*****");
+
+
+
+        // reverse right half pyramid of stars in the output.
+        System.out.println("\n\n*****\n****\n***\n**\n*");
+        
+
+        // Left Half Pyramid.
+        System.out.println("\n\n    *\n   **\n  ***\n ****\n*****");
+
+
+
+    }
+    
+}
+
+    
+
