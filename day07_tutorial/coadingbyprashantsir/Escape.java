19ac977a8213f982a6a2cb3b6ab5c8be67e35517
samyogp <samyogphuyal036@gmail.com>
2026-02-20 10:12:26 +0545

    Added Escape sequence example

---
+++ day07_tutorial/coadingbyprashantsir/Escape.java
@@ -0,0 +1,13 @@
+package day07_tutorial.coadingbyprashantsir;
+
+public class Escape {
+    public static void main(String[] args) {
+
+        // in this case ecape sequence is used to print double quotes in the output.
+        
+        System.out.println("HEllo \"World\".....");
+
+
+    }
+    
+}
