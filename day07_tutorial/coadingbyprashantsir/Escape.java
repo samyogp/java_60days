65654a91bfd5bab47bb2b2e14678012c503b013a
samyogp <samyogphuyal036@gmail.com>
2026-02-20 18:07:52 +0545

    example of \n to go next line

--- day07_tutorial/coadingbyprashantsir/Escape.java
+++ day07_tutorial/coadingbyprashantsir/Escape.java
@@ -12,7 +12,9 @@ public class Escape {
         //  double backslash is used to print single backslash in the output.
         System.out.println("hello \\Coading World\\.....");
 
-
+     // to go on next line we can use \n escape sequence.
+     System.out.println("Hello coding world\n welcome to java programming");
+     
 
     }
     
