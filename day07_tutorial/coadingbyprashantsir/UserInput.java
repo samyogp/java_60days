5ba94028bb4c1ce2feefe12a5e0cbc7bf0ad0698
samyogp <samyogphuyal036@gmail.com>
2026-02-20 22:11:25 +0545

    code updated

--- day07_tutorial/coadingbyprashantsir/UserInput.java
+++ day07_tutorial/coadingbyprashantsir/UserInput.java
@@ -8,6 +8,12 @@ public class UserInput {
         System.out.print("Please Enter Your Name:");
         String name = input.nextLine();
         System.out.println("Hello " + name + " What are you doing there...!" );
+        System.out.println(name + " , Also tell me about your age:");
+        int age = input.nextInt();
+        System.out.println("Wow " + name + " you are " + age + " years old ..!");
+        // scanner closed
+         input.close();
+
 
     }
     
