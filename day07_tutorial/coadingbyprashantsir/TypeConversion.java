e4fdcf1393b6653be1800eb1d04852bdc40e70ee
samyogp <samyogphuyal036@gmail.com>
2026-02-20 22:46:28 +0545

    code updated and making integer from float using type casting

--- day07_tutorial/coadingbyprashantsir/TypeConversion.java
+++ day07_tutorial/coadingbyprashantsir/TypeConversion.java
@@ -4,6 +4,11 @@ public class TypeConversion {
     public static void main(String[] args) {
         float myFloat = 5;
         System.out.println(myFloat);
+
+
+        // example making integer
+        int myInt = (int) 5.0f;
+        System.out.println(myInt);
     }
     
 }
