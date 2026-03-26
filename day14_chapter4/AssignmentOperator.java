e5c70517529e09448fccc463ac09bfb37410108a
samyogp <samyogphuyal036@gmail.com>
2026-03-26 18:36:09 +0545

    adding single line comments in code

--- day14_chapter4/AssignmentOperator.java
+++ day14_chapter4/AssignmentOperator.java
@@ -1,4 +1,5 @@
 package day14_chapter4;
+// Assignment operator Example 
 
 public class AssignmentOperator {
     public static void main(String[] args) {
@@ -6,6 +7,8 @@ public class AssignmentOperator {
         int newInt = myInt;
         System.out.println("myInt: " + myInt);
         System.out.println("newInt: " + newInt);
+
+        // 9 = myInt = newInt; // this is not valid because 9 is a constant value and cannot be assigned to a variable
     }
     
 }
