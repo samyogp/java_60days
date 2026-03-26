bb9b2c69ca6ac66088d03407e775633bb3dbff13
samyogp <samyogphuyal036@gmail.com>
2026-03-26 18:56:19 +0545

    adding new subfolder arithmetic_operator and doing first example ArithmeticDemo1.java

---
+++ day14_chapter4/arithmetic_operator/ArithmeticDemo1.java
@@ -0,0 +1,17 @@
+package day14_chapter4.arithmetic_operator;
+
+public class ArithmeticDemo1 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a/b);
+        System.out.println(a*b);
+    }
+
+    
+    
+}
