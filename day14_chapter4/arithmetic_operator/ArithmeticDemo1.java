fff3ea23f60c69486ce6d9bad5d3e5f88041262c
samyogp <samyogphuyal036@gmail.com>
2026-03-26 18:58:00 +0545

    adding another example in ArithmeticDemo1 file

--- day14_chapter4/arithmetic_operator/ArithmeticDemo1.java
+++ day14_chapter4/arithmetic_operator/ArithmeticDemo1.java
@@ -10,6 +10,14 @@ public class ArithmeticDemo1 {
         System.out.println(a-b);
         System.out.println(a/b);
         System.out.println(a*b);
+
+        double x = 5;
+        double y = 2;
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x/y);
+        System.out.println(x*y);
+
     }
 
     
