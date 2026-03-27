5111a96f8bdae57f7a086769d25253859fa9707e
samyogp <samyogphuyal036@gmail.com>
2026-03-27 08:46:22 +0545

    adding new Demo of Arithmetic operator

---
+++ day14_chapter4/arithmetic_operator/ArithmeticDemo2.java
@@ -0,0 +1,25 @@
+package day14_chapter4.arithmetic_operator;
+
+public class ArithmeticDemo2 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+
+        // division by zero will throw an ArithmeticException
+        // System.out.println(a / 0);
+
+
+
+
+
+    }
+    
+}
---
+++ day14_chapter4/arithmetic_operator/OrderOfOperationDemo.java
@@ -0,0 +1,11 @@
+package day14_chapter4.arithmetic_operator;
+
+public class OrderOfOperationDemo {
+public static void main(String[] args) {
+    System.out.println(10 + 5 * 2); // multiplication is performed before addition, so this will print 20
+    System.out.println((10 + 5) * 2); // parentheses change the order of operations, so this will print 30
+    System.out.println(10 + 5 * 2 - 3); // multiplication is
+}
+    
+
+}
