93ece90f7c82409c6797fc411c8074a2d54bd3b0
samyogp <samyogphuyal036@gmail.com>
2026-03-27 15:56:59 +0545

    adding current value in statement and then increment the value

---
+++ day14_chapter4/unary_operator/UnaryOperatorDemo5.java
@@ -0,0 +1,17 @@
+package day14_chapter4.unary_operator;
+
+public class UnaryOperatorDemo5 {
+   // use the current value in our statement and then increment the value by 1.
+   public static void main(String[] args) {
+    int a = 5;
+    System.out.println(a);
+    System.out.println(a++);
+
+
+    int b = 10;
+    System.out.println(b);
+    System.out.println(b++);
+    
+   } 
+    
+}
