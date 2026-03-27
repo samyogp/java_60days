1983d4250d2a11d42d65333ec1873b6f9f819189
samyogp <samyogphuyal036@gmail.com>
2026-03-27 15:50:37 +0545

    increment the value by 1 and then used it in our statement

---
+++ day14_chapter4/unary_operator/UnaryOperatorDemo3.java
@@ -0,0 +1,20 @@
+package day14_chapter4.unary_operator;
+
+public class UnaryOperatorDemo3 {
+    public static void main(String[] args) {
+        
+
+
+    // Increment the value by 1 and then use it in our statement.
+    int a = 10;
+    System.out.println(a);
+    System.out.println(++a);
+
+    int b = 20;
+    System.out.println(b);
+    System.out.println(b++);
+    
+    
+    
+}
+}
