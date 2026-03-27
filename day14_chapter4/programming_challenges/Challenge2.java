39f3270133f4d740c45f7183d729c26d5393a619
samyogp <samyogphuyal036@gmail.com>
2026-03-27 16:29:01 +0545

    adding new programming challenge and completed these

---
+++ day14_chapter4/programming_challenges/Challenge2.java
@@ -0,0 +1,28 @@
+package day14_chapter4.programming_challenges;
+
+public class Challenge2 {
+    // create a program to calculate product of two floating point numbers.
+    public static void main(String[] args) {
+        // System.out.println("Enter the first number A: ");
+        // java.util.Scanner sc = new java.util.Scanner(System.in);
+        // float a = sc.nextFloat();
+
+        // System.out.println("Enter the second number B:");
+        // float b = sc.nextFloat();
+
+        // System.out.println("A * B = " + (a * b));
+        // sc.close();
+
+        System.out.println("Enter the first number A: ");
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        float a = sc.nextFloat();
+
+        System.out.println("Enter the second number B: ");
+        float b = sc.nextFloat();
+
+        System.out.println("A * B = " + (a * b));
+        sc.close();
+
+    }
+    
+}
