1612bad6192ac794049391eea6f8cabf940648fd
samyogp <samyogphuyal036@gmail.com>
2026-03-27 15:46:18 +0545

    adding unary operator examples converting positive value into negative and negative value into positive value

---
+++ day14_chapter4/unary_operator/UnaryOperatorDemo2.java
@@ -0,0 +1,20 @@
+package day14_chapter4.unary_operator;
+
+public class UnaryOperatorDemo2 {
+    // converts positive value into negative value.
+    public static void main(String[] args) {
+        // positive value is converted into negative value by using unary minus operator (-)
+
+        int a = 5;
+        System.out.println(a);
+        System.out.println(-a);
+       
+        // negative value converted into positive value by using unary minus operator (-)
+        
+        int b = -12;
+        System.out.println(b);
+        System.out.println(-b);
+
+    }
+    
+}
