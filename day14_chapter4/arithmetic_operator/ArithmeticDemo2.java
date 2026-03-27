0acbad9157906476347d5ef4048adad81e64ba47
samyogp <samyogphuyal036@gmail.com>
2026-03-27 08:52:43 +0545

    addinng new code in code

--- day14_chapter4/arithmetic_operator/ArithmeticDemo2.java
+++ day14_chapter4/arithmetic_operator/ArithmeticDemo2.java
@@ -2,23 +2,32 @@ package day14_chapter4.arithmetic_operator;
 
 public class ArithmeticDemo2 {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 5;
+        // int a = 10;
+        // int b = 5;
 
 
 
-        System.out.println(a + b);
-        System.out.println(a - b);
-        System.out.println(a * b);
-        System.out.println(a / b);
-        System.out.println(a % b);
+        // System.out.println(a + b);
+        // System.out.println(a - b);
+        // System.out.println(a * b);
+        // System.out.println(a / b);
+        // System.out.println(a % b);
 
         // division by zero will throw an ArithmeticException
         // System.out.println(a / 0);
 
-
-
-
+     // where user can input the values and perform the operations
+     System.out.println("Enter first number A: ");
+       java.util.Scanner sc = new java.util.Scanner(System.in);
+       int a = sc.nextInt();
+       System.out.println("Enter secomd number B:");
+         int b = sc.nextInt();
+            System.out.println("A + B = " + (a + b));
+            System.out.println("A - B = " + (a - b));
+             System.out.println("A * B = " + (a * b));
+              System.out.println("A / B = " + (a / b));
+               System.out.println("A % B = " + (a % b));
+               
 
     }
     
