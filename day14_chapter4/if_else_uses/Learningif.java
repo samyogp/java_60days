14ce11b1a81bcfe5432242a303dc9b9795201512
samyogp <samyogphuyal036@gmail.com>
2026-03-29 16:06:56 +0545

    adding new folder if_else_uses

---
+++ day14_chapter4/if_else_uses/Learningif.java
@@ -0,0 +1,20 @@
+package day14_chapter4.if_else_uses;
+
+public class Learningif {
+    public static void main(String[] args) {
+        
+    
+
+    boolean isMale = true;
+    String name = "bob";
+     System.out.println("before if");
+    if (isMale) { 
+        System.out.println("MR." + name);
+
+        System.out.println("after if");
+        
+
+    }
+    
+}
+}
