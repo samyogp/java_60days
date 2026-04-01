73c997eef83f3701f830d8a3b49be1d55bd21525
samyogp <samyogphuyal036@gmail.com>
2026-04-01 15:41:38 +0545

    this is the another example where user can input the number and check from the among number are negative or positive

--- day14_chapter4/if_else_uses/iflearning.java
+++ day14_chapter4/if_else_uses/iflearning.java
@@ -1,17 +1,35 @@
 package day14_chapter4.if_else_uses;
 
+import java.util.Scanner;
+
 public class iflearning {
     
 
     public static void main(String[] args) {
-        int number = 10;
-        if (number > 0) {
-            System.out.println("The number is positive.");
-        } else if (number < 0) {
-            System.out.println("The number is negative.");
-        } else {
-            System.out.println("The number is zero.");
-        }
-    }
-    
+//         int number = 10;
+//         if (number > 0) {
+//             System.out.println("The number is positive.");
+//         } else if (number < 0) {
+//             System.out.println("The number is negative.");
+//         } else {
+//             System.out.println("The number is zero.");
+//         }
+//     }
+
+ // user can input number and check if positive or negative or Zero);
+
+Scanner scanner = new Scanner(System.in);
+System.out.print("Enter a number: ");
+int number = scanner.nextInt();
+
+if (number > 0) {
+    System.out.println("The number is positive.");
+} else if (number < 0) {
+    System.out.println("The number is negative.");
+} else {
+    System.out.println("The number is zero.");
+    scanner.close();
+
+}
+}
 }
