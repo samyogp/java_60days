e2a6d730195cce145695a24d1dd58cbb863bb2d3
samyogp <samyogphuyal036@gmail.com>
2026-04-01 15:49:00 +0545

    relational operator Demo example

---
+++ day14_chapter4/relational_operator/RelationalOperatorDemo.java
@@ -0,0 +1,17 @@
+package day14_chapter4.relational_operator;
+
+public class RelationalOperatorDemo {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+
+        System.out.println("a > b: " + (a > b));   // false
+        System.out.println("a < b: " + (a < b));   // true
+        System.out.println("a >= b: " + (a >= b)); // false
+        System.out.println("a <= b: " + (a <= b)); // true
+        System.out.println("a == b: " + (a == b)); // false
+        System.out.println("a != b: " + (a != b)); // true
+    }
+    
+    
+}
