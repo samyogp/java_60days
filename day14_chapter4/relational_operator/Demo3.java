04fa10881b26eba315950ed03bcf51132dd70bf5
samyogp <samyogphuyal036@gmail.com>
2026-04-01 16:04:22 +0545

    adding new marriage iligiblity for the confirmation which time is best for marriage

---
+++ day14_chapter4/relational_operator/Demo3.java
@@ -0,0 +1,23 @@
+package day14_chapter4.relational_operator;
+
+import java.util.Scanner;
+
+public class Demo3 {
+
+    // eligible for marriage or not using if and else statement.
+    public static void main(String[] args) {
+       Scanner input = new Scanner(System.in);
+       System.out.println("Welcome to the marriage eligiblity checker!");
+       System.out.println("please enter your name: ");
+       String name = input.nextLine();
+       System.out.println("please enter your age: ");
+        int age = input.nextInt();
+
+        if (age >= 18) {
+            System.out.println("congratulations " + name + "! you are eligible for marriage.");
+        } else {
+            System.out.println("sorry " + name + " you are not eligible for marriage.");
+            input.close();
+        }
+        }
+        }
