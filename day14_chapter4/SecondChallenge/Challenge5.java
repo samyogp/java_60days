5638b70588ccbc1f2f347aa6d11824cc16199078
samyogp <samyogphuyal036@gmail.com>
2026-04-03 17:05:35 +0545

    code updated

--- day14_chapter4/SecondChallenge/Challenge5.java
+++ day14_chapter4/SecondChallenge/Challenge5.java
@@ -7,23 +7,4 @@ public class Challenge5 {
 
     public static void main(String[] args) {
 
-        Scanner input = new Scanner(System.in);
-System.out.println("Welcome to age calculator!");
-System.out.println("Please enter your age:");
-int age = input.nextInt();
-
-if (age < 0) {
-    System.out.println("Invalid age entered!");
-} else if (age >= 65) {
-    System.out.println("You are a senior citizen");
-} else if (age >= 20) {
-    System.out.println("You are an adult");
-} else if (age >= 13) {
-    System.out.println("You are a teenager");
-} else {
-    System.out.println("You are a child");
-}
-
-input.close();
-}
-}
+        
