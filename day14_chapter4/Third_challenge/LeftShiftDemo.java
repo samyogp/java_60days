ebfb72d26fcf1b05cb41d1c44e7835b844d8bc88
samyogp <samyogphuyal036@gmail.com>
2026-04-07 00:17:41 +0545

    adding left shift Demo

--- day14_chapter4/Third_challenge/LeftShiftDemo.java
+++ day14_chapter4/Third_challenge/LeftShiftDemo.java
@@ -1,5 +1,25 @@
 package day14_chapter4.Third_challenge;
 
+import java.util.Scanner;
+
 public class LeftShiftDemo {
+     public static void main(String[] args) {
+        
+    
+
+      Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to showcase of Bitwise complement operator");
+        System.out.println("please enter your number");
+        int num1 = input.nextInt();
+
+        int result = num1 << 1;
+
+        System.out.println("Your result is: " + result);
+
+    }
+
+
+
     
 }
+
