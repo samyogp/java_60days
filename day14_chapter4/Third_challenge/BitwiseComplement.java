bd143e6255d34eef124496e83d51f8ec07ff335a
samyogp <samyogphuyal036@gmail.com>
2026-04-07 00:03:39 +0545

    adding bitwise complement Demo in github

--- day14_chapter4/Third_challenge/BitwiseComplement.java
+++ day14_chapter4/Third_challenge/BitwiseComplement.java
@@ -1,11 +1,23 @@
 package day14_chapter4.Third_challenge;
 
+import java.util.Scanner;
+
 public class BitwiseComplement {
     public static void main(String[] args) {
-        // create a program that shows bitwise complement of a number.
-        int number = 5; // Example number
-        int complement = ~number; // Perform bitwise complement operation
-        System.out.println("The bitwise complement of " + number + " is: " + complement);
+       
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to showcase of Not/Compliment operator");
+        System.out.println("please enter your number");
+        int num = input.nextInt();
+
+        int result = ~num; 
+
+        System.out.println("Your result is: " + result);
+
     }
+
+
+
     
 }
