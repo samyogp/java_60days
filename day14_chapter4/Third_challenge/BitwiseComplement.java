b009a639269e62607ba7797e371a4cd581d49a85
samyogp <samyogphuyal036@gmail.com>
2026-04-06 23:59:40 +0545

    adding

---
+++ day14_chapter4/Third_challenge/BitwiseComplement.java
@@ -0,0 +1,11 @@
+package day14_chapter4.Third_challenge;
+
+public class BitwiseComplement {
+    public static void main(String[] args) {
+        // create a program that shows bitwise complement of a number.
+        int number = 5; // Example number
+        int complement = ~number; // Perform bitwise complement operation
+        System.out.println("The bitwise complement of " + number + " is: " + complement);
+    }
+    
+}
