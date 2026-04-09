344ad5543b07ef89a1ca92ae5bd78a722ee97203
samyogp <samyogphuyal036@gmail.com>
2026-04-09 16:59:28 +0545

    displaying sum of each number

--- day16_q43to50/question43.java
+++ day16_q43to50/question43.java
@@ -1,11 +1,32 @@
 package day16_q43to50;
 
+import java.util.Scanner;
+
 public class question43 {
     // Q43: Enter elements in matrix m x n, display sum of each row and column [BIM 2018]
-    public static void main(String[] args) {
-        // This question is similar to Q42, but we will calculate and display the sum of each row and column instead of the total sum.
-        // The code structure will be similar to Q42, but with additional logic for row and column sums.
+     static boolean isPrime(int n) {
+        if (n < 2) return false;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
     }
 
-    
-}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter start: ");
+        int start = sc.nextInt();
+        System.out.print("Enter end: ");
+        int end = sc.nextInt();
+
+        System.out.println("Prime numbers between " + start + " and " + end + ":");
+        for (int i = start; i <= end; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+       
+
+
+    }
