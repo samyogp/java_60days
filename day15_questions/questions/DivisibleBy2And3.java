0422ca5efa1a913789a660d58b39da74965bae6f
samyogp <samyogphuyal036@gmail.com>
2026-04-09 16:38:13 +0545

    adding program on the displaying the size of array

---
+++ day15_questions/questions/DivisibleBy2And3.java
@@ -0,0 +1,26 @@
+package day15_questions.questions;
+
+import java.util.Scanner;
+
+public class DivisibleBy2And3 {
+// Q41: Enter numbers in array of size n, display those divisible by 2 and 3 [BIM 2017]
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        System.out.println("Enter " + n + " numbers:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Numbers divisible by both 2 and 3:");
+        for (int i = 0; i < n; i++) {
+            if (arr[i] % 2 == 0 && arr[i] % 3 == 0) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+    }
+    
+}
