629f8d4f661091e1afe5556c70c188c24c7d31af
samyogp <samyogphuyal036@gmail.com>
2026-04-10 21:46:07 +0545

    comments were added

---
+++ day16_q43to50/question49.java
@@ -0,0 +1,15 @@
+package day16_q43to50;
+
+public class question49 {
+    static void print(int n, int current) {
+        if (current > n) return;
+        System.out.print(current + " ");
+        print(n, current + 1);
+    }
+
+    public static void main(String[] args) {
+        int n = 10;
+        System.out.print("Numbers 1 to " + n + ": ");
+        print(n, 1);
+    }
+}
