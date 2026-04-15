b5e2b8d2a8ce76150be9b3694725eb2ea1c2da83
samyogp <samyogphuyal036@gmail.com>
2026-04-16 00:17:13 +0545

    adding new java program of if and else

---
+++ day14_chapter4/if_else_uses/Demo1.java
@@ -0,0 +1,28 @@
+package day14_chapter4.Demo1;
+
+import java.util.Scanner;
+
+public class Demo1 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your marks: ");
+        int marks = input.nextInt();
+
+        if (marks < 0 || marks > 100) {
+            System.out.println("Invalid marks");
+        } else if (marks >= 80) {
+            System.out.println("Distinction");
+        } else if (marks >= 60) {
+            System.out.println("First Division");
+        } else if (marks >= 50) {
+            System.out.println("Second Division");
+        } else if (marks >= 40) {
+            System.out.println("Third Division");
+        } else {
+            System.out.println("Fail");
+        }
+
+        input.close();
+    }
+}
