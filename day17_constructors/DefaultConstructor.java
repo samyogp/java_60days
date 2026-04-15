1ad3fb6d72bcb6bd29dcf5725c8fbd425484d30c
samyogp <samyogphuyal036@gmail.com>
2026-04-15 15:26:50 +0545

    addingnew constructor folder and also added inside that folder files

---
+++ day17_constructors/DefaultConstructor.java
@@ -0,0 +1,23 @@
+package day17_constructors;
+
+
+class Car {
+    String color;
+    int speed;
+
+    Car() {
+        color = "White";
+        speed = 0;
+        System.out.println("car Created ");
+
+    }
+}
+
+public class DefaultConstructor {
+    public static void main(String[] args) {
+        Car C = new Car();
+        System.out.println(C.color); // white
+        
+    }
+    
+}
