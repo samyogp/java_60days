8c06e3289834889b9e3fcc23b5f0e037162af90e
samyogp <samyogphuyal036@gmail.com>
2026-04-15 15:30:49 +0545

    the main examples added

---
+++ day17_constructors/ParameterizedConstructor.java
@@ -0,0 +1,28 @@
+package day17_constructors;
+
+import java.awt.Color;
+import javax.sound.sampled.SourceDataLine;
+
+
+
+
+class Car {
+    String color;
+    int speed;
+
+    Car() {
+    
+
+
+    Color = "White";
+    speed = 0;
+      system.out.println("car Created ");
+}
+}
+
+public class ParameterizedConstructor {
+    public static void main(String[] args) {
+        Car C = new Car();
+        System.out.println(C.color); // white       
+    }
+    }
